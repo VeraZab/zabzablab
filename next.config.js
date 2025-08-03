b4b9bbf5e6f669ee7863ce525bbc1601ab7a9e0f
VeraZab <vera.zabeida@gmail.com>
2025-08-03 17:30:35 -0400

    about img fixes

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    // Disable the new dev overlay if you find it distracting
-    devOverlay: false
-  }
-}
-
-module.exports = nextConfig
