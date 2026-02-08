b98aa753d65e362a7ad2a691b955655bee43fc37
VeraZab <vera.zabeida@gmail.com>
2026-02-07 20:29:29 -0500

    images work

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: { unoptimized: true },
     // If your site is at username.github.io/repo-name, set basePath: '/repo-name'
     // basePath: '/zabzablab',
 }
