a7cbec00e59305d12deb92dbeefccbe2b3e4ad9d
VeraZab <vera.zabeida@gmail.com>
2026-02-14 22:04:09 -0500

    new logo

--- app/components/header/Logo.tsx
+++ app/components/header/Logo.tsx
@@ -2,11 +2,23 @@
 
 import Link from 'next/link'
 import styles from '/styles/header.module.css'
+import { zabzablabLogoPaths, LOGO_VIEWBOX } from './zabzablabLogoPaths'
 
 export const Logo = ({ isMobile }: { isMobile?: boolean }) => {
     return (
         <Link href="/" className={styles.logoLink} aria-label="ZabZabLab home">
-            <img src="/assets/zabzablab.png" alt="ZabZabLab" />
+            <svg
+                className={styles.logoImg}
+                viewBox={LOGO_VIEWBOX}
+                preserveAspectRatio="xMidYMid meet"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden
+                fill="currentColor"
+            >
+                {zabzablabLogoPaths.map((d, i) => (
+                    <path key={i} d={d} fill="currentColor" stroke="none" />
+                ))}
+            </svg>
         </Link>
     )
 }
